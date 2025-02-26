1d642ae68a414cdeb3326fb0e33f481820e30ae7
ebeard02 <beardew@mail.uc.edu>
2025-02-26 11:00:33 -0500

    started added bpv code

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pvlib import pvsystem
-from pvlib import bifacial
+from pvlib.bifacial.pvfactors import pvfactors_timeseries
+import warnings
+
+pvrow_height = 1
+pvrow_width = 4
+pitch = 10
+gcr = pvrow_width / pitch
+axis_azimuth = 180
+albedo = 0.2
 
 module_parameters = {'pdc0': 5000, 'gamma_pdc': -0.004}
-
 inverter_parameters = {'pdc0': 5000, 'eta_inv_nom': 0.96}
 
 system = pvsystem.PVSystem(inverter_parameters=inverter_parameters,
                            module_parameters=module_parameters,name='Test System')
+
 
 pdc = system.pvwatts_dc(g_poa_effective=1000, temp_cell=30)
 
