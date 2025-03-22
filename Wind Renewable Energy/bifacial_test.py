7136c81fbc1e8b55d51d1f81c1f0aabccaecaa09
ebeard02 <beardew@mail.uc.edu>
2025-03-22 18:29:00 -0400

    Added code to compare bPV and mPV AC power

--- Wind Renewable Energy/bifacial_test.py
+++ Wind Renewable Energy/bifacial_test.py
@@ -11,11 +11,15 @@ import matplotlib.pyplot as plt
 warnings.filterwarnings(action='ignore', module='pvfactors')
 
 # create site location and times characteristics
-lat, lon = 36.084, -79.817
-tz = 'Etc/GMT+5'
-times = pd.date_range('2021-06-21', '2021-6-22', freq='1min', tz=tz)
+locations = {
+    'latitude': [58.4546, -26.3167, -0.0206],
+    'longitude': [-134.1739, 31.1333, 109.3414],
+    'timezone': ['America/Juneau', 'Africa/Mbabane', 'Asia/Pontianak'],
+    'name': ['Juneau, Alaska (USA)', 'Mbabane, Eswatini', 'Pontianak, Indonesia']
+}
 
-
+# create system locations and times as dataframes
+locations_df = pd.DataFrame(locations)
 
 # create site system characteristics
 axis_tilt = 0
@@ -34,77 +38,88 @@ cec_module = cec_modules['Trina_Solar_TSM_300DEG5C_07_II_']
 cec_inverters = pvsystem.retrieve_sam('cecinverter')
 cec_inverter = cec_inverters['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
 
-# create a location for site, and get solar position and clearsky data
-site_location = location.Location(lat, lon, tz=tz, name='Greensboro, NC')
-solar_position = site_location.get_solarposition(times)
-cs = site_location.get_clearsky(times)
-
-# load solar position and tracker orientation for use in pvsystem object
 sat_mount = pvsystem.SingleAxisTrackerMount(axis_tilt=axis_tilt,
                                             axis_azimuth=axis_azimuth,
                                             max_angle=max_angle,
                                             backtrack=True,
                                             gcr=gcr)
 
-# created for use in pvfactors timeseries
-orientation = sat_mount.get_orientation(solar_position['apparent_zenith'],
+# set up figure for plot
+figure, axis = plt.subplots(len(locations_df['name']),1)
+
+for index, site in locations_df.iterrows():
+
+    lat = site['latitude']
+    lon = site['longitude']
+    tz = site['timezone']
+    times = pd.date_range('2021-06-21', '2021-6-22', freq='1min', tz=tz)
+
+    # create a location for site, and get solar position and clearsky data
+    site_location = location.Location(lat, lon, tz=tz, name = site['name'])
+    solar_position = site_location.get_solarposition(times)
+    cs = site_location.get_clearsky(times)
+
+    orientation = sat_mount.get_orientation(solar_position['apparent_zenith'],
                                         solar_position['azimuth'])
 
-# get rear and front side irradiance from pvfactors transposition engine
-# explicity simulate on pvarray with 3 rows, with sensor placed in middle row
-# users may select different values depending on needs
-irrad = pvfactors_timeseries(solar_position['azimuth'],
-                             solar_position['apparent_zenith'],
-                             orientation['surface_azimuth'],
-                             orientation['surface_tilt'],
-                             axis_azimuth,
-                             times,
-                             cs['dni'],
-                             cs['dhi'],
-                             gcr,
-                             pvrow_height,
-                             pvrow_width,
-                             albedo,
-                             n_pvrows=3,
-                             index_observed_pvrow=1
-                             )
+    # get rear and front side irradiance from pvfactors transposition engine
+    # explicity simulate on pvarray with 3 rows, with sensor placed in middle row
+    # users may select different values depending on needs
+    irrad = pvfactors_timeseries(solar_position['azimuth'],
+                                solar_position['apparent_zenith'],
+                                orientation['surface_azimuth'],
+                                orientation['surface_tilt'],
+                                axis_azimuth,
+                                times,
+                                cs['dni'],
+                                cs['dhi'],
+                                gcr,
+                                pvrow_height,
+                                pvrow_width,
+                                albedo,
+                                n_pvrows=3,
+                                index_observed_pvrow=1
+                                )
 
-# turn into pandas DataFrame
-irrad = pd.concat(irrad, axis=1)
 
-# create bifacial effective irradiance using aoi-corrected timeseries values
-irrad['effective_irradiance'] = (
-    irrad['total_abs_front'] + (irrad['total_abs_back'] * bifaciality)
-)
+    # dc arrays
+    array = pvsystem.Array(mount=sat_mount,
+                        module_parameters=cec_module,
+                        temperature_model_parameters=temp_model_parameters)
 
-# dc arrays
-array = pvsystem.Array(mount=sat_mount,
-                       module_parameters=cec_module,
-                       temperature_model_parameters=temp_model_parameters)
+    # create system object
+    system = pvsystem.PVSystem(arrays=[array],
+                            inverter_parameters=cec_inverter)
+    
+    # turn into pandas DataFrame
+    irrad = pd.concat(irrad, axis=1)
 
-# create system object
-system = pvsystem.PVSystem(arrays=[array],
-                           inverter_parameters=cec_inverter)
+    # create bifacial effective irradiance using aoi-corrected timeseries values
+    irrad['effective_irradiance'] = (
+        irrad['total_abs_front'] + (irrad['total_abs_back'] * bifaciality)
+    )
 
-# ModelChain requires the parameter aoi_loss to have a value. pvfactors
-# applies surface reflection models in the calculation of front and back
-# irradiance, so assign aoi_model='no_loss' to avoid double counting
-# reflections.
-mc_bpv = modelchain.ModelChain(system, site_location, aoi_model='no_loss')
-mc_bpv.run_model_from_effective_irradiance(irrad)
+    # ModelChain requires the parameter aoi_loss to have a value. pvfactors
+    # applies surface reflection models in the calculation of front and back
+    # irradiance, so assign aoi_model='no_loss' to avoid double counting
+    # reflections.
+    mc_bpv = modelchain.ModelChain(system, site_location, aoi_model='no_loss')
+    mc_bpv.run_model_from_effective_irradiance(irrad)
 
-#create irradiance for front face only
-irrad['effective_irradiance'] = (
-    irrad['total_abs_front']
-)
+    # create irradiance for front face only
+    # aoi value is set to 'physical' in this case
+    irrad['effective_irradiance'] = (
+        irrad['total_abs_front']
+    )
 
-mc_mpv = modelchain.ModelChain(system, site_location, aoi_model='physical' )
-mc_mpv.run_model_from_effective_irradiance(irrad)
+    mc_mpv = modelchain.ModelChain(system, site_location, aoi_model='physical' )
+    mc_mpv.run_model_from_effective_irradiance(irrad)
 
-# plot results
-plt.plot(times, mc_bpv.results.ac, 'r')
-plt.plot(times, mc_mpv.results.ac, 'b')
-plt.title(site_location.name)
-plt.xlabel('Time')
-plt.ylabel('AC Power (W)')
+    # plot results
+    axis[index].plot(times, mc_bpv.results.ac, 'r', times, mc_mpv.results.ac, 'b')
+    axis[index].set_title(site_location.name)
+    axis[index].set_ylabel('AC Power (W)')
+    axis[index].set_xlabel('Time')
+
+plt.tight_layout()
 plt.show()
